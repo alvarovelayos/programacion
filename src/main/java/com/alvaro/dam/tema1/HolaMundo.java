dced44a4ad824f177a8a574911223fdb6f03ebab
alvarovelayos <alvarovelayos31@gmail.com>
2022-10-21 22:26:12 +0200

    Comienzo de proyecto

---
+++ src/main/java/com/alvaro/dam/tema1/HolaMundo.java
@@ -0,0 +1,13 @@
+package com.alvaro.dam.tema1;
+
+/**
+ * Hello world!
+ *
+ */
+public class HolaMundo 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/alvaro/dam/tema1/UsoMatematicas.java
@@ -0,0 +1,10 @@
+package com.alvaro.dam.tema1;
+
+public class UsoMatematicas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
