c490e6f496f5bc783134322fd0b9bb58dfee62c5
alvarovelayos <alvarovelayos31@gmail.com>
2022-10-22 20:06:18 +0200

    subida operadores relacionales

---
+++ src/main/java/com/alvaro/dam/tema1/OperadoresRelacionales.java
@@ -0,0 +1,21 @@
+package com.alvaro.dam.tema1;
+
+public class OperadoresRelacionales {
+
+	public static void main(String[] args) {
+		
+		double op1,op2;
+		char op3,op4;
+		op1=1.34;
+		op2=1.35;
+		op3='a';
+		op4='b';
+		System.out.println("op1=" + op1 + "op2=" + op2);
+		System.out.println("op1>op2 = " + (op1 > op2));
+		System.out.println("op1<op2 =" + (op1 < op2));
+		System.out.println("op1==op2 = " + (op1==op2));
+		System.out.println("op1!=op2 = " + (op1!=op2));
+		System.out.println("'a'>'b' = " + (op3>op4));
+	}
+
+}
