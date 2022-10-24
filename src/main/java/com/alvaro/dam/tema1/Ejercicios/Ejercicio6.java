ac0dfa27535663896e46ab23de70c937d9c2b06f
alvarovelayos <alvarovelayos31@gmail.com>
2022-10-24 22:24:46 +0200

    subido ejercicio 6

--- src/main/java/com/alvaro/dam/tema1/Ejercicios/Ejercicio6.java
+++ src/main/java/com/alvaro/dam/tema1/Ejercicios/Ejercicio6.java
@@ -1,15 +1,28 @@
 package com.alvaro.dam.tema1.Ejercicios;
-
+import java.util.Scanner;
 //6.	Dibuja un ordinograma que dado el precio de un artículo 
 //y el precio de venta real nos muestre el porcentaje de descuento realizado.
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
 	
-		double precioArticulo = 100;
-		double precioVentaReal = 80;
+		double precioArticulo;
+		double precioVentaReal;
 		double descuento;
+		double porcentaje;
 		
-		System.out.println("");
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Introduce el precio del articulo");
+		precioArticulo = entrada.nextDouble();
+		System.out.println("Introduce el precio con descuento");
+		precioVentaReal = entrada.nextDouble();
+		
+		descuento = precioArticulo - precioVentaReal;
+		porcentaje = (descuento * 100) / precioArticulo; 
+		
+		System.out.println("Precio articulo sin descuentro: " + precioArticulo);
+		System.out.println("Precio articulo con descuento: " + precioVentaReal);
+		System.out.println("El porcentaje del articulo es: " + porcentaje + " % ");
+		
 	}
 }
